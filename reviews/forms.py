6001e323e3c039166f64cfa5498b863d83b04a17
StienBoodts <stien.boodts@gmail.com>
2023-01-16 21:47:58 +0000

    created add reviews functionality and added button to product page

--- reviews/forms.py
+++ reviews/forms.py
@@ -8,7 +8,7 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = '__all__'
-    
+        
     image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
